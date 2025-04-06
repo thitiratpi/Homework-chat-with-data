fa21a9aa7c46c811ef275ffcb9438f630f53340f
thitiratpi <pinktoice_ice@hotmail.com>
2025-04-06 17:18:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ st.title("🐧 My Chatbot and Data Analysis App")
 st.subheader("Conversation and Data Analysis")
 
 # Capture Gemini API Key
-gemini_api_key = st.text_input(
-    "Gemini API Key: ", placeholder="Type your API Key here...", type="password"
+#gemini_api_key = st.text_input(
+    #"Gemini API Key: ", placeholder="Type your API Key here...", type="password"
 )
 
 # Initialize the Gemini Model
@@ -17,7 +17,7 @@ if gemini_api_key:
     try:
         # Configure Gemini with the provided API Key
         genai.configure(api_key=gemini_api_key)
-        model = genai.GenerativeModel("gemini-pro")
+        model = genai.GenerativeModel("gemini-2.0-flash-lite")
         st.success("Gemini API Key successfully configured.")
     except Exception as e:
         st.error(f"An error occurred while setting up the Gemini model: {e}")
